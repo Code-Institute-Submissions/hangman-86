af9153b2778190e41a06a36287142730c39f5b68
Sebastiano Ballotta <sebeballotta@outlook.com>
2022-11-12 13:33:08 +0100

    Define game logic inside the hangman_game function

--- run.py
+++ run.py
@@ -1,5 +1,7 @@
 # All code or resources used in this project are credited in the README file.
 import random
+import string
+
 from words import word_list
 from time import sleep
 from hangman import lives_left
@@ -26,6 +28,7 @@ def player_nickname():
     """
     Retrieves the player's nickname. It will be used to give a feedback
     """
+    global nickname
     while True:
         nickname = input("\nWho's playing today? ")
         if nickname.isalpha():
@@ -50,4 +53,49 @@ def hangman_game():
     while counting on the number of lives left
     """
 
+    welcome_rules()
+    player_nickname()
+    word = get_word(word_list)
+    letters_needed = set(word)  # letters in the word
+    alphabet = set(string.ascii_uppercase)
+    letters_guessed = set()  # letters user guesses
 
+    lives = 10
+
+    while len(letters_needed) > 0 and lives > 0:
+        print("\nYou've used these letters: ", ' '.join(sorted(letters_guessed)))
+        print('\nLives left:', lives, )
+
+        word_guess = [letter if letter in letters_guessed else '_' for letter in word]
+        print(messages.green + lives_left[lives])
+        print('Current word: ', ' '.join(word_guess))
+        print('\n----------------------------------------')
+
+        user_guess = input('\nPlease guess a letter: ').upper()
+
+        if user_guess in alphabet - letters_guessed:
+            letters_guessed.add(user_guess)
+            if user_guess in letters_needed:
+                letters_needed.remove(user_guess)
+                print('')
+
+            else:
+                lives = lives - 1
+                print('\nOps,', user_guess, 'is not in the word.')
+
+        elif user_guess in letters_guessed:
+            print("\nYou've guessed this letter already. Please try again.")
+
+        else:
+            print("That doesn't work, please type in a valid letter, A-Z.")
+
+    # player is hanged
+    if lives == 0:
+        print(messages.red + lives_left[lives])
+        print(messages.bold + f"Oh no, {nickname.capitalize()}, you've been hanged!")
+        print("The word was" + messages.red, word)
+    else:
+        print(messages.bold + f"Congratulations {nickname.capitalize()}!")
+        print("You're right, the word was " + word)
+
+hangman_game()
